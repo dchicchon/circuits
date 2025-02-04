b8d0d974d55b9196c8dac1d9f0b201f6cb839723
Daniel Chicchon <danielchicchon@gmail.com>
2025-02-04 15:44:57 -0800

    added deletion functionality to canvas

---
+++ src/utils/classes.tsx
@@ -0,0 +1,183 @@
+import Q5 from 'q5xjs';
+import { useStore } from '@/utils/store';
+import { modes } from '@/utils/modes';
+import { nanoid } from 'nanoid';
+
+export interface Nodes {
+  [id: string]: Node;
+}
+interface NodeData {
+  pos: Q5.Vector;
+  boundary: string;
+  diameter?: number;
+  width?: number;
+  height?: number;
+}
+
+export class Drawing {
+  nodes: Nodes;
+  sketch: Q5;
+  hovered: string | null;
+  selected: string | null;
+  mainSketch: HTMLElement | null;
+  debug: boolean;
+  height: number;
+  width: number;
+  canvas: HTMLElement;
+
+  constructor() {
+    this.sketch = new Q5();
+    this.sketch.draw = () => {
+      this.draw();
+    };
+    this.sketch.setup = () => {
+      this.setup();
+    };
+    this.sketch.mouseClicked = () => {
+      this.mouseClicked();
+    };
+    this.selected = null;
+    this.hovered = null;
+    this.nodes = useStore.getState().nodes;
+    this.mainSketch = document.getElementById('main_sketch');
+    this.debug = true;
+    this.height = document.body.clientHeight;
+    this.width = document.body.clientWidth;
+    this.canvas = this.sketch.createCanvas(this.width, this.height);
+    this.sketch.pixelDensity(window.devicePixelRatio);
+    if (this.mainSketch) {
+      this.mainSketch.appendChild(this.canvas);
+    }
+  }
+
+  setup() {
+    this.sketch.frameRate(10);
+    this.sketch.stroke('white');
+    this.sketch.strokeWeight(1);
+  }
+
+  draw() {
+    this.sketch.background('#242424');
+    this.drawNodes();
+  }
+
+  mouseClicked() {
+    const mode = useStore.getState().mode;
+    if (mode === modes.SELECT) {
+      if (this.hovered === this.selected) {
+        this.selectNode('');
+      } else if (this.hovered) {
+        this.selectNode(this.hovered);
+      } else {
+        this.selectNode('');
+      }
+    } else if (mode === modes.ADD_CIRCUIT_NODE) {
+      this.addCircuitNode(this.sketch.mouseX, this.sketch.mouseY);
+    }
+  }
+
+  selectNode(id: string) {
+    const setSelected = useStore.getState().setSelected;
+    setSelected(id);
+  }
+
+  // we want to save our nodes in our store
+  // todo: should nodes draw themselves? might be easier?
+  drawNodes() {
+    this.hovered = null;
+    this.sketch.cursor(this.sketch.ARROW);
+    Object.keys(this.nodes).forEach((id) => {
+      const node = this.nodes[id];
+      if (node.boundary === 'circle') {
+        this.sketch[node.boundary](node.pos.x, node.pos.y, node.diameter);
+      }
+      if (node.boundary === 'rect') {
+        this.sketch[node.boundary](node.pos.x, node.pos.y, node.width, node.height);
+      }
+      if (node.isHovered(this.sketch.mouseX, this.sketch.mouseY)) {
+        this.hovered = node.id;
+        this.sketch.cursor(this.sketch.HAND);
+      }
+      // todo: add dashed lines around this
+
+      const selected = useStore.getState().selected;
+      if (node.id === selected) {
+        this.sketch.push();
+        this.sketch.noFill();
+        if (node.boundary === 'circle') {
+          this.sketch[node.boundary](node.pos.x, node.pos.y, node.diameter + 15);
+        }
+        // draw selection around this
+        this.sketch.pop();
+      }
+    });
+  }
+
+  /**
+   * A node can be selectable.
+   * A node must have an assigned shape
+   */
+  addNode(node: Node) {
+    this.nodes[node.id] = node;
+  }
+
+  deleteNode(id: string) {
+    delete this.nodes[id];
+  }
+
+  createVector(x: number, y: number) {
+    return this.sketch.createVector(x, y);
+  }
+
+  addCircuitNode(x: number, y: number) {
+    const node = new Node({
+      boundary: 'circle',
+      pos: this.createVector(x, y),
+      diameter: 25,
+    });
+    this.addNode(node);
+  }
+}
+
+export class Node {
+  pos: Q5.Vector;
+  id: string;
+  boundary: string;
+  width?: number;
+  height?: number;
+  diameter?: number | null;
+  minX: number;
+  maxX: number;
+  minY: number;
+  maxY: number;
+
+  constructor(data: NodeData) {
+    this.id = nanoid();
+    this.pos = data.pos;
+    this.boundary = data.boundary;
+    if (this.boundary === 'circle' && data.diameter) {
+      this.diameter = data.diameter;
+      this.minX = this.pos.x - data.diameter / 2;
+      this.minY = this.pos.y - data.diameter / 2;
+      this.maxX = this.pos.x + data.diameter / 2;
+      this.maxY = this.pos.y + data.diameter / 2;
+    } else {
+      this.minX = this.pos.x;
+      this.minY = this.pos.y;
+      this.maxX = this.pos.x + data.width;
+      this.maxY = this.pos.y + data.height;
+    }
+  }
+
+  isHovered(mouseX: number, mouseY: number): boolean {
+    if (
+      mouseX > this.minX &&
+      mouseX < this.maxX &&
+      mouseY > this.minY &&
+      mouseY < this.maxY
+    ) {
+      return true;
+    }
+    return false;
+  }
+}
--- src/utils/store.tsx
+++ src/utils/store.tsx
@@ -1,32 +1,15 @@
 import { create } from 'zustand';
-import { Canvas, Circle, FabricObject, Point } from 'fabric';
-// import * as fabric from 'fabric';
-import { nanoid } from 'nanoid';
+import { Nodes, Node, Drawing } from './classes';
 import { modes } from './modes';
 
-/**
- * modes
- * select mode
- * create circuit node mode
- */
-
-// a node is an item that is on the page
-// interface Node {
-//   type: string;
-//   data: object;
-//   canvasRef: FabricObject;
-// }
-
 interface State {
-  selected: string | null;
+  drawing: Drawing | null;
+  setDrawing: (val: Drawing) => void;
+  selected: string;
   setSelected: (val: string) => void;
   deleteNode: () => void;
-  items: object;
-  setItem: (id, object) => void;
-  addCircuitNode: (point: Point) => void;
-  canvasAction: (mousePos: Point) => void;
-  canvas: Canvas | null;
-  setCanvas: (val: Canvas | null) => void;
+  nodes: Nodes;
+  setNodes: (id: string, node: Node) => void;
   mode: string;
   setMode: (value: string) => void;
   openBar: boolean;
@@ -39,104 +22,25 @@ interface State {
 }
 
 export const useStore = create<State>((set, get) => ({
-  canvas: null,
-  items: {},
-  setItem: () => set(() => ({})),
-  setCanvas: (val: Canvas | null) => set(() => ({ canvas: val })),
-  addCircuitNode: (mousePos: Point) =>
-    set((state) => {
-      if (state.canvas) {
-        // const id = nanoid();
-        // const node = {
-        //   ref: new Circle({
-        //     radius: 10,
-        //     fill: 'white',
-        //     top: mousePos.y,
-        //     left: mousePos.x,
-        //     id,
-        //   }),
-        // };
-
-        // lets do our own thing
-        // node.ref.on('mousedown:before', () => {
-        //   console.log('selected node');
-        //   // how do I add a ring around to
-        //   // specify selection?
-        //   //   state.setSelected(id);
-        // });
-
-        // node.ref.on('selected', () => {
-        //   state.setSelected(id);
-        // });
-
-        // figure out how to add this to all
-        // node.ref.setControlsVisibility({
-        //   mt: false,
-        //   mb: false,
-        //   ml: false,
-        //   mr: false,
-        //   bl: false,
-        //   br: false,
-        //   tl: false,
-        //   tr: false,
-        //   mtr: false,
-        // });
-        state.canvas.add(node.ref);
-        state.items[id] = node;
-      }
-      return {
-        items: state.items,
-      };
-    }),
-  connectCircuitNodes: () =>
-    set((state) => {
-      console.log('connect circuit nodes');
-      return {};
-    }),
-  selected: null,
+  drawing: null,
+  setDrawing: (val: Drawing) => set(() => ({ drawing: val })),
+  nodes: {},
+  setNodes: () => set(() => ({})),
+  selected: '',
   setSelected: (id: string) =>
     set(() => ({
       selected: id,
     })),
-  deleteNode: () =>
-    set((state) => {
-      const selected = state.selected;
-      if (selected) {
-        const list = state.canvas?.getActiveObjects();
-        list?.forEach((node) => {
-          delete state.items[node.id];
-        });
-        state.canvas.remove(...list);
-        // node.ref.dispose();
-        // delete state.items[selected];
-      }
-      return {
-        items: state.items,
-      };
-    }),
-
-  // do actions with the canvas
-  canvasAction: (mousePos: Point) => {
-    const mode = get().mode;
-    const addCircuitNode = get().addCircuitNode;
-    const setSelected = get().setSelected;
-    // we should have an action tree to complete on the canvas
-    switch (mode) {
-      case modes.ADD_CIRCUIT_NODE:
-        addCircuitNode(mousePos);
-        // addCircuitNode(canvas, mousePos);
-        break;
-      case modes.SELECT:
-        // console.log('select');
-        break;
-      case modes.CONNECT_CIRCUIT_NODE:
-        console.log('connect circuit mode');
-        break;
-    }
+  deleteNode: () => {
+    const drawing = get().drawing;
+    const selected = get().selected;
+    drawing?.deleteNode(selected);
+    // delete an item on the drawing?
   },
   mode: modes.SELECT,
   setMode: (value: string) => set(() => ({ mode: value })),
 
+  //   Messaging
   openBar: false,
   setOpenBar: (bool: boolean) => set(() => ({ openBar: bool })),
   message: '',
